9a0862290d5c35aca41fd11d2795b8c1ad1329aa
cristiannZzzz <cristian.bulla02@gmail.com>
2025-11-05 15:38:35 -0300

    ajuste en constantes

--- frontend/src/constants/mapConfig.js
+++ frontend/src/constants/mapConfig.js
@@ -1,15 +1,15 @@
 // 🎯 Coordenadas del Campus Guayacán - Universidad Católica del Norte
 export const UCN_COQUIMBO_BOUNDS = [
-  [-29.96672, -71.356201], // Suroeste
-  [-29.96328, -71.346792], // Noreste
+  [-29.967362, -71.355547], // Suroeste
+  [-29.963179, -71.346513], // Noreste
 ];
 
-export const UCN_CAMPUS_CENTER = [-29.965, -71.35103]; // Centro calculado
+export const UCN_CAMPUS_CENTER = [-29.965335, -71.3519868]; // Centro calculado
 
 export const MAP_ZOOM_LIMITS = {
-  min: 16, // Más zoom out para ver contexto
-  max: 19, // Mantener máximo zoom para detalles
-  default: 17, // Zoom inicial más alejado para ver todo el campus
+  min: 17.5, // Más zoom out para ver contexto
+  max: 19.5, // Mantener máximo zoom para detalles
+  default: 18, // Zoom inicial más alejado para ver todo el campus
 };
 
 export const GEO_SERVER_CONFIG = {
