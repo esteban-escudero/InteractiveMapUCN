43da51fdeb6f0ee0152527b20e29653bc8df918d
esteban-escudero <esteban.escudero@alumnos.ucn.cl>
2026-01-09 11:30:42 -0300

    mapaUniversidad

--- frontend/src/config/app.js
+++ frontend/src/config/app.js
@@ -13,26 +13,11 @@ export const GEO_SERVER_CONFIG = {
   layerName: process.env.REACT_APP_GEO_SERVER_LAYER || 'edificio',
 };
 
-/*export const MAP_CONFIG = {
-  bounds: [
-    [-29.967316, -71.355622],
-    [-29.963208, -71.346738], 
-  ],
-  zoom: {
-    min: 19, 
-    max: 19,
-    default: 19, 
-  },
-};*/
-
-//Coordenadas de Prueba
 export const MAP_CONFIG = {
   bounds: [
-    [-30.81757, -71.26728],
-    [-30.84844, -71.24359],
+    [-29.967316, -71.355622],
+    [-29.963208, -71.346738],
   ],
-
-
   zoom: {
     min: 19,
     max: 19,
